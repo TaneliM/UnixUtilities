a1eff244da77a7bfc04683ca7e25e3ebd985dff9
TaneliM <tane.my@gmail.com>
2021-04-21 16:01:09 +0300

    Initial version of my-cat + README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+DESCRIPTION
+Simple C implementations of existing utilities.
+
+
+MY-CAT
+A modified version of: https://github.com/TaneliM/ReverseCat
+Prints the constents of all files provided as arguments.
+
+USAGE (my-cat)
+./my-cat (file1.txt) (file2.txt) ... ... (file3.txt)
+
+
+MY-GREP
+wip
+
+
+MY-ZIP
+wip
+
+
+MY-UNZIP
+wip
+
+
+TESING
+Tests can be found at: https://github.com/remzi-arpacidusseau/ostep-projects/tree/master/initial-utilities
---
+++ my-cat.c
@@ -0,0 +1,113 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+// List node struct
+struct lines
+{
+	char* string;
+	struct lines * prev;
+	struct lines * next;
+};
+
+
+// Reading input to the list
+int read_lines(struct lines** line, FILE* stream) {
+	
+	char* buffer = NULL;
+	size_t n = 0;
+	struct lines* lp;
+	
+	// reading input stream line by line
+	while (getline(&buffer, &n , stream) >= 0) {
+		if (buffer == NULL) {
+			fprintf(stderr, "getline failed\n");
+		}
+		
+		// Allocating memory for the first node
+		if (*line == NULL) { 
+			if ((*line = lp = malloc(sizeof(struct lines))) == NULL) {
+				fprintf(stderr, "malloc failed\n");
+				return -1;
+			}
+			
+			lp->prev = lp->next = NULL;
+		}
+		// Allocating memory for a new node
+		else { 
+			if ( (lp->next = malloc(sizeof(struct lines))) == NULL) {
+				fprintf(stderr, "malloc failed\n");
+				return -1;
+			}
+
+			lp->next->prev = lp;
+			lp = lp->next;
+		}
+		
+		// Allocating memory for the line to store it in the list
+		if ( (lp->string = malloc(strlen(buffer) + 1)) == NULL) {
+			fprintf(stderr, "malloc failed\n");
+			return -1;
+		}
+		strcpy(lp->string, buffer);
+	}
+	return 0;
+}
+
+
+// Printing the list
+void print_lines(struct lines * line, FILE * stream) {
+	struct lines * lp;
+	lp = line;
+	
+	// Writing the list to the "stream" in reverse
+	while (lp != NULL) {
+		fprintf(stream, "%s", lp->string);
+		lp = lp->next;
+	}
+}
+
+
+// Deleting the linked list
+void delete_lines(struct lines * line) {
+	struct lines * lp;
+	lp = line;
+	
+	// Freeing allocated memory
+	while (lp != NULL) {
+		line = lp->next;
+		free(lp->string);
+		free(lp);
+		lp = line;
+	}
+}
+
+
+// Handling the arguments
+int main(int argc, char** argv) {
+	struct lines * line = NULL;
+	FILE* pFile;
+	
+	if (argc > 1) {	
+		// Going through all defined files
+		for (int i = 1; i < argc; i++) {
+			if ((pFile = fopen(argv[i], "r")) == NULL) {
+				fprintf(stdout, "wcat: cannot open file\n");
+				exit(1);
+			}
+			
+			if (read_lines(&line, pFile) == -1) {
+				exit(1);
+			}
+			
+			print_lines(line, stdout);
+			delete_lines(line);
+			
+			fclose(pFile);
+		}
+	}
+	return 0;
+}
+
+
